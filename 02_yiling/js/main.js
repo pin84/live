1629f0918cfaf751949b837878594fef778d9883
qp1984 <qp1984@live.com>
2017-10-08 18:59:59 +0800

    02_yinling

--- 02_yiling/js/main.js
+++ 02_yiling/js/main.js
@@ -4,53 +4,70 @@
 
 $(function () {
     let index = 0;//全局变量，用于控制 menu部分的 a 标签的背景色
-    const menu = $('.menu ul li');
-    const left = $('.left');
-    const right = $('.right');
+    let menu = $('.menu ul li');
+    let left = $('.left');
+    let right = $('.right');
 
-    const bg = $('.bg ul li');
+    let imgList = $('.imgList li');
+    let indicator = $('.indicator li');
 
 
-    right.click(function () {
-        index++;
-        if (index === bg.length) {
-            index = 0
-        }
-        change();
-    });
+    // right.click(function () {
+    //     index++;
+    //     if (index === active.length) {
+    //         index = 0
+    //     }
+    //     change();
+    // });
+    //
+    // left.click(function () {
+    //     index--;
+    //     if (index === -1) {
+    //         index = active.length - 1;
+    //     }
+    //     change();
+    // });
+    //
 
-    left.click(function () {
-        index--;
-        if (index === -1) {
-            index = bg.length - 1;
-        }
-        change();
-    });
-
-    setInterval(function () {
-        index++;
-        if (index === bg.length) {
-            index = 0
-        }
-        change();
-    }, 1000);
-
-    function change() {
-        //menu.eq(index).addClass("li_bg").siblings().removeClass("li_bg");
-        bg.eq(index).addClass("current").siblings().removeClass("current");
+    /*公共方法*/
+    function change(el) {
+        el.eq(index).addClass("current").siblings().removeClass("current");
     }
 
     // change();
 
+    /*菜单 menu*/
     for (let i = 0; i < menu.length; i++) {
         menu[i].addEventListener('mouseover', function () {
             index = $(this).index();
-            //index = i;
-            menu.eq(index).addClass("li_bg").siblings().removeClass("li_bg");
+            // menu.eq(index).addClass("active").siblings().removeClass("active");
+            change(menu);
         })
     }
 
+    /*小圆点 indicator*/
+    for (let i = 0; i < indicator.length; i++) {
+        indicator[i].addEventListener('click',function () {
+            index = $(this).index();
+            // indicator.eq(index).addClass("current").siblings().removeClass("current");
+            change(indicator);
+            change(imgList);
+        })
+    }
 
+    /*自动轮播*/
+    /*
+    * setTimeout: 只执行一次
+    * setInterval: 根据设定的时间重复执行
+    * */
+    setInterval(function () {
+        index++;
+        if (index === imgList.length) {
+            index = 0
+        }
+        change(imgList);
+        change(indicator);
+    }, 10000);
 });
 
 
